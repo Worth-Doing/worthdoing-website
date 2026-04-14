9ddd168e44be10220f2854206fdcd4d273803dbc
simon-pierre boucher <simon-pierreboucher@BIBL-MKGAG2-P.ul.ca>
2026-04-14 15:25:39 -0400

    fix: Allow worthdoing.ai as dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["www.worthdoing.ai", "worthdoing.ai"],
 };
 
 export default nextConfig;
